0660ae6b2d3a860c11396ee788715fd6fceb9ae7
Dan Brajkovic <dbrajkovic@mac.com>
2012-10-09 21:13:59 -0500

    Refactored MMLoginView

--- MobMonkey/MobMonkey/MMLoginViewController.m
+++ MobMonkey/MobMonkey/MMLoginViewController.m
@@ -12,12 +12,18 @@
 #import "SVProgressHUD.h"
 #import "MMClientSDK.h"
 
-@interface MMLoginViewController ()
+@interface MMLoginViewController () {
+    UITextField *emailTextField;
+    UITextField *passwordTextField;
+}
+
+@property (nonatomic, retain) NSMutableArray *contentList;
+
+- (void)showAlertView:(NSString*)message;
 
 @end
 
 @implementation MMLoginViewController
-@synthesize contentList = _contentList;
 
 - (id)initWithStyle:(UITableViewStyle)style
 {
@@ -86,53 +92,13 @@
 {
     static NSString *CellIdentifier = @"Cell";
     UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier];
-    
-    cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+    if (!cell) {
+        cell = [[UITableViewCell alloc]initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+    }
     [cell.contentView addSubview:[_contentList objectAtIndex:indexPath.row]];
-    
     cell.selectionStyle = UITableViewCellSelectionStyleNone;
-    
     return cell;
 }
-
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:[NSArray arrayWithObject:indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    }   
-    else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
-{
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
 
 #pragma mark - Table view delegate
 
