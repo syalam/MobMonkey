5105b465aee66efb1deb6935d790cd3043a5e0cc
Dan Brajkovic <dbrajkovic@mac.com>
2012-10-16 01:51:20 -0500

    Click on location in Locations List takes you to detail

--- MobMonkey/MobMonkey/MMLocationsViewController.m
+++ MobMonkey/MobMonkey/MMLocationsViewController.m
@@ -141,13 +141,7 @@
 
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
 {
-    // Navigation logic may go here. Create and push another view controller.
-    /*
-     <#DetailViewController#> *detailViewController = [[<#DetailViewController#> alloc] initWithNibName:@"<#Nib name#>" bundle:nil];
-     // ...
-     // Pass the selected object to the new view controller.
-     [self.navigationController pushViewController:detailViewController animated:YES];
-     */
+     [[MMClientSDK sharedSDK]locationScreen:self locationDetail:[self.locations objectAtIndex:indexPath.section]];
 }
 
 @end
