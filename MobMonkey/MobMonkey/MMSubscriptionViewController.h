271fc7dd9499dce37aa6d01f125b602b5668ba63
Scott Menor <samenor@me.com>
2012-12-10 13:06:30 +0100

    stubbing out subscription view controller

---
+++ MobMonkey/MobMonkey/MMSubscriptionViewController.h
@@ -0,0 +1,13 @@
+//
+//  MMSubscriptionViewController.h
+//  MobMonkey
+//
+//  Created by Scott Menor on 10/12/12.
+//  Copyright (c) 2012 Reyaad Sidique. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MMSubscriptionViewController : ViewController
+
+@end
