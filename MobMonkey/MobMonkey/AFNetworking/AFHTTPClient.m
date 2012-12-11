324c1b66a78b57085d4dc770c36c4592d39900fa
Scott Menor <samenor@me.com>
2012-12-11 23:46:27 +0100

    upload progress w/ HUD

--- MobMonkey/MobMonkey/AFNetworking/AFHTTPClient.m
+++ MobMonkey/MobMonkey/AFNetworking/AFHTTPClient.m
@@ -619,7 +619,24 @@ static void AFNetworkReachabilityReleaseCallback(const void *info) {
 {
 	NSURLRequest *request = [self requestWithMethod:@"POST" path:path parameters:parameters];
 	AFHTTPRequestOperation *operation = [self HTTPRequestOperationWithRequest:request success:success failure:failure];
-    [self enqueueHTTPRequestOperation:operation];
+  [operation setUploadProgressBlock:^(NSUInteger bytesWritten, long long totalBytesWritten, long long totalBytesExpectedToWrite) {
+    CGFloat progress = (CGFloat)totalBytesWritten / totalBytesExpectedToWrite;
+    // TODO / FIXME - add progress bar
+    // TODO / FIXME - only do this for sufficiently large posts
+    NSLog(@"totalBytesExpectedToWrite: %lld", totalBytesExpectedToWrite);
+    [SVProgressHUD showProgress:progress];
+    NSLog(@"upload progress: %f", progress);
+  }];
+  
+  [operation setCompletionBlockWithSuccess:^(AFHTTPRequestOperation *operation, id responseObject) {
+    [SVProgressHUD dismiss];
+  } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+    [SVProgressHUD dismiss];
+    
+    [SVProgressHUD showErrorWithStatus:@"Couldn't complete upload."];
+  }];
+  
+  [self enqueueHTTPRequestOperation:operation];
 }
 
 - (void)putPath:(NSString *)path 
