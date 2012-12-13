379496413525cda193c67ab5dccfba906a758746
Scott Menor <samenor@me.com>
2012-12-13 13:30:14 +0100

    merge of basic scaffold and changed long press to tap gesture recognizer

--- MobMonkey/MobMonkey/MMMapFilterViewController.h
+++ MobMonkey/MobMonkey/MMMapFilterViewController.h
@@ -7,7 +7,12 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <MapKit/MapKit.h>
 
-@interface MMMapFilterViewController : UIViewController
+@interface MMMapFilterViewController : UIViewController <MKMapViewDelegate> {
+  MKMapView *mapView;
+}
+
+@property (strong, nonatomic) IBOutlet MKMapView *mapView;
 
 @end
--- MobMonkey/MobMonkey/MMMapFilterViewController.m
+++ MobMonkey/MobMonkey/MMMapFilterViewController.m
@@ -22,23 +22,50 @@
 
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
+  self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+  if (self) {
+    // Custom initialization
+  }
+  return self;
 }
 
 - (void)viewDidLoad
 {
-    [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+  [super viewDidLoad];
+  
+  NSLog(@"map filter view did load");
+  // disable user interaction for map
+  mapView.scrollEnabled = NO;
+  mapView.zoomEnabled = NO;
+  
+  mapView.delegate = self;
+  
+  // add gesture recognizer
+  UITapGestureRecognizer *tapGestureRecognizer = [[UITapGestureRecognizer alloc]
+                                               initWithTarget:self
+                                               action:@selector(handleTap:)];
+
+  [mapView addGestureRecognizer:tapGestureRecognizer];
+}
+
+- (void)handleTap:(UIGestureRecognizer *)gestureRecognizer
+{
+  CGPoint touchPoint = [gestureRecognizer locationInView:mapView];
+  CLLocationCoordinate2D touchMapCoordinate = [mapView convertPoint:touchPoint toCoordinateFromView:mapView];
+  MKPointAnnotation *pointAnnotation = [[MKPointAnnotation alloc] init];
+  pointAnnotation.coordinate = touchMapCoordinate;
+  pointAnnotation.title = @"Hello";
+  [mapView addAnnotation:pointAnnotation];
+  
+/*  UIActionSheet *actionSheet = [[UIActionSheet alloc] initWithTitle:@"title" delegate:self cancelButtonTitle:@"Cancel" destructiveButtonTitle:@"destructive" otherButtonTitles:@"other titles", nil];
+  
+  [actionSheet showInView:mapView];*/
 }
 
 - (void)didReceiveMemoryWarning
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
 }
 
 @end
