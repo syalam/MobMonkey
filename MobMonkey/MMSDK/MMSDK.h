2a165f962f5319b4222c8fd9ef57d5f9bc3b31ca
mrsidique <reyaad.sidique@gmail.com>
2013-02-13 00:28:43 -0700

    created static sdk library
    Signed-off-by: mrsidique <reyaad.sidique@gmail.com>

---
+++ MobMonkey/MMSDK/MMSDK.h
@@ -0,0 +1,13 @@
+//
+//  MMSDK.h
+//  MMSDK
+//
+//  Created by Reyaad Sidique on 2/13/13.
+//  Copyright (c) 2013 Reyaad Sidique. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MMSDK : NSObject
+
+@end
---
+++ MobMonkey/MMSDK/MMSDK.m
@@ -0,0 +1,13 @@
+//
+//  MMSDK.m
+//  MMSDK
+//
+//  Created by Reyaad Sidique on 2/13/13.
+//  Copyright (c) 2013 Reyaad Sidique. All rights reserved.
+//
+
+#import "MMSDK.h"
+
+@implementation MMSDK
+
+@end
